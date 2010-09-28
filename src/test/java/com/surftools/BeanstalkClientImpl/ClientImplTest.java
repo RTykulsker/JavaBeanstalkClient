ffa44e23299a2f28da88f7891217715a28dad06a
Bob Tykulsker <rtykulsker@gmail.com>
2010-09-28 07:19:59 -0700

    dual license, BSD 3-clause, per Scott Andreas

--- src/test/java/com/surftools/BeanstalkClientImpl/ClientImplTest.java
+++ src/test/java/com/surftools/BeanstalkClientImpl/ClientImplTest.java
@@ -2,24 +2,25 @@ package com.surftools.BeanstalkClientImpl;
 
 /*
 
- Copyright 2009 Robert Tykulsker 
+Copyright 2009-2010 Robert Tykulsker 
 
- This file is part of JavaBeanstalkCLient.
+This file is part of JavaBeanstalkCLient.
 
- JavaBeanstalkCLient is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+JavaBeanstalkCLient is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version, or alternatively, the BSD license supplied
+with this project in the file "BSD-LICENSE".
 
- JavaBeanstalkCLient is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+JavaBeanstalkCLient is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with JavaBeanstalkCLient.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with JavaBeanstalkCLient.  If not, see <http://www.gnu.org/licenses/>.
 
- */
+*/
 
 import java.io.Serializable;
 import java.util.ArrayList;
